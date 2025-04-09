85c07ba77ae926754681ada1d137d5959242a030
Manliketerry008 <103952634+Manliketerry008@users.noreply.github.com>
2025-04-09 23:17:30 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,102 @@
+const products = [
+  {
+    id: 1,
+    subject: "English",
+    location: "London",
+    price: 100,
+    spaces: 10,  
+    rating: 2,
+    image: "https://media.istockphoto.com/id/1437535145/vector/english-hand-drawn-doodles-and-lettering.jpg?s=612x612&w=0&k=20&c=77N8ZSTa0n31YMQlWU17xFqaeOV4rVnS6IQijdtu8BI="
+  },
+  {
+    id: 2,
+    subject: "Math",
+    location: "New York",
+    price: 120,
+    spaces: 10,  
+    rating: 4,
+    image: "https://via.placeholder.com/150"
+  },
+
+  {
+    id: 4,
+    subject: "History",
+    location: "Berlin",
+    price: 110,
+    spaces: 10,  
+    rating: 3,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 5,
+    subject: "Art",
+    location: "Paris",
+    price: 95,
+    spaces: 10,  
+    rating: 5,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 6,
+    subject: "Music",
+    location: "Los Angeles",
+    price: 130,
+    spaces: 10,  
+    rating: 4,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 7,
+    subject: "Literature",
+    location: "Rome",
+    price: 105,
+    spaces: 10,  
+    rating: 4,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 8,
+    subject: "Physics",
+    location: "Moscow",
+    price: 115,
+    spaces: 10,  
+    rating: 5,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 9,
+    subject: "Geography",
+    location: "Tokyo",
+    price: 85,
+    spaces: 10,  
+    rating: 3,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 10,
+    subject: "Philosophy",
+    location: "Athens",
+    price: 120,
+    spaces: 10,  
+    rating: 4,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 11,
+    subject: "Economics",
+    location: "Sydney",
+    price: 95,
+    spaces: 10,  
+    rating: 2,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 12,
+    subject: "Psychology",
+    location: "Cape Town",
+    price: 125,
+    spaces: 10,  
+    rating: 5,
+    image: "https://via.placeholder.com/150"
+  }
+];
